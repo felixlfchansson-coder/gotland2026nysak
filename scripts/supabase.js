0b9256fee2b1e42e32444f419668d4856c0adf17
Felix <felix.lfc.hansson@gmail.com>
2026-05-13 00:41:59 +0200

    nu hoppas jag

--- scripts/supabase.js
+++ scripts/supabase.js
@@ -1,5 +1,5 @@
 const supabaseUrl = "https://xeretvarnzfrplzljkvt.supabase.co";
-const supabaseKey = "sb_publishable_aePOQWv07JVNYuqoUzeNlA_KIwvjMnX";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InhlcmV0dmFybnpmcnBsemxqa3Z0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Nzg0MzMwNTQsImV4cCI6MjA5NDAwOTA1NH0.e-kEnEVEEnmSeBMdq6K2SCY6VxvOUnppGy5HL2Psd-I";
 
 const supabaseClient = supabase.createClient(
   SUPABASE_URL,
